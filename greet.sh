61af075545c51d2566a8d98b52c59d97c5a31e36
Andrius Juodagalvis <andrius.juodagalvis@tfai.vu.lt>
2019-09-23 03:40:43 -0400

    pavyzdys

--- greet.sh
+++ greet.sh
@@ -2,3 +2,5 @@
 rm -f greetings.tex
 echo "Greetings from `whoami`@`hostname` on `date`" > greetings.tex
 lualatex test.tex
+lualatex test.tex # recompile to get references right
+rm -f greetings.tex
