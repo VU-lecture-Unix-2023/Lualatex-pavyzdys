d51c919d5662e4178c263df199c24780d6fb7f43
Andrius Juodagalvis <andrius.juodagalvis@tfai.vu.lt>
2024-10-17 00:22:41 +0300

    update for 2024

--- greet.sh
+++ greet.sh
@@ -5,8 +5,11 @@ lualatex test.tex
 lualatex test.tex # recompile to get references right
 
 pdflatex test2.tex
-pdflatex test2.tex
+pdflatex test2.tex # recompile to get references right
 
-evince test.pdf test2.pdf &
+pdflatex test3.tex
+pdflatex test3.tex # recompile to get references right
+
+evince test.pdf test2.pdf test3.pdf &
 
 rm -f greetings.tex
